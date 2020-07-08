749defd29f3a3f4d5b78f068a9cb404de8630318
Utpal Maiti <Utpalmaiti12@gmail.com>
2020-07-08 16:38:35 +0530

    Add project files.

---
+++ OAuthDemonstration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OAuthDemonstration.Startup))]
+namespace OAuthDemonstration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
